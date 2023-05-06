332289980ed9ca369060770882d8f93e0f7deefd
Orkhan <mammedov.01@mail.ru>
2023-05-07 00:53:15 +0400

    Hero part with adaptive slider has done

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,17 @@
 import './HomePage.scss'
+import HeroPart from './components/HeroPart/HeroPart';
+
+
 
 // Uncomment this when you start to work
-// export const HomePage = () => {
-//     return (
+export const HomePage = () => {
+    return (
+        <>
+        <HeroPart />
+    
 
-//     );
-// }
+        </>
+    );
+}
 
-// export default HomePage
+export default HomePage
