8bba7662a16315bc3cd9a8c07b5d8434836dbbd8
cemile <ovciyeva04@gmail.com>
2023-05-31 18:35:00 +0400

    Login page has been added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Header } from './shared/Header/Header';
+import {LoginPage} from './pages/LoginPage/LoginPage';
 import Footer from './shared/Footer/Footer';
 import HomePage from './pages/HomePage/HomePage';
 import React from 'react';
@@ -7,9 +8,10 @@ import React from 'react';
 function App() {
   return (
     <>
-      <Header />
-      <HomePage />
-      <Footer />
+      {/* <Header /> */}
+      <LoginPage/>
+      {/* <HomePage />
+      <Footer /> */}
     </>
   );
 }
