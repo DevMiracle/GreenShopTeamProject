a6acc351dd590433b020635d38bf6d3ccc6074e0
Orkhan <mammedov.01@mail.ru>
2023-06-05 00:12:05 +0400

    ShoppingCart with style and discount functionality

--- src/pages/CartPage/components/ShoppingCart.tsx
+++ src/pages/CartPage/components/ShoppingCart.tsx
@@ -1,6 +1,6 @@
 import './ShoppingCart.scss';
 import { CartItem, useCart } from './CartContext';
-import React from 'react';
+import React, { useState } from 'react';
 import remove_button from '../assets/Delete.svg';
 
 interface CartItemProps {
@@ -45,11 +45,39 @@ const CartItemComponent: React.FC<CartItemProps> = ({
 
 export const ShoppingCart = () => {
   const { cartItems, addToCart, removeFromCart, updateQuantity, calculateTotalPrice } = useCart();
+  const [promoCode, setPromoCode] = useState('');
+  const [discountApplied, setDiscountApplied] = useState(false);
+  const [couponDiscount, setCouponDiscount] = useState(0);
+
+  const applyPromoCode = () => {
+    if (promoCode === 'ANTOSHKA') {
+      const totalPrice = calculateTotalPrice();
+      const discountAmount = totalPrice * 0.1;
+      setCouponDiscount(discountAmount);
+      setDiscountApplied(true);
+    }
+  };
+
+  const calculateSubtotal = () => {
+    const totalPrice = calculateTotalPrice();
+    const subtotal = totalPrice - couponDiscount;
+    return subtotal > 0 ? subtotal.toFixed(2) : '0.00';
+  };
+
+  const handleQuantityChange = (id: number, quantity: number) => {
+    updateQuantity(id, quantity);
+
+    if (discountApplied) {
+      const totalPrice = calculateTotalPrice();
+      const discountAmount = totalPrice * 0.1;
+      setCouponDiscount(discountAmount);
+    }
+  };
 
   return (
     <div className="container">
       <div className="top">
-        <span>Products</span>
+        <span className="products-title">Products</span>
         <span className="price-title">Price</span>
         <span className="quantity-title">Quantity</span>
         <span className="total-title">Total</span>
@@ -62,18 +90,54 @@ export const ShoppingCart = () => {
           <CartItemComponent
             key={cartItem.id}
             cartItem={cartItem}
-            increaseQuantity={() => updateQuantity(cartItem.id, cartItem.quantity + 1)}
-            decreaseQuantity={() => updateQuantity(cartItem.id, Math.max(1, cartItem.quantity - 1))}
+            increaseQuantity={() => handleQuantityChange(cartItem.id, cartItem.quantity + 1)}
+            decreaseQuantity={() => handleQuantityChange(cartItem.id, Math.max(1, cartItem.quantity - 1))}
             removeFromCart={() => removeFromCart(cartItem.id)}
           />
         ))
       )}
       {cartItems.length > 0 && (
         <div className="cart-summary">
-          <h3>Cart Summary</h3>
-          <p>Total Items: {cartItems.length}</p>
-          <p>Total Price: ${calculateTotalPrice().toFixed(2)}</p>
-          <button>Checkout</button>
+          <p>Cart Totals</p>
+          <div className="line2"></div>
+          <div className="cart-main">
+            <p className="coupon">Coupon Apply</p>
+            <div className="coupon-input">
+              <input
+                type="text"
+                placeholder="Enter coupon code here..."
+                value={promoCode}
+                onChange={(e) => setPromoCode(e.target.value)}
+              />
+              <button onClick={applyPromoCode}>Apply</button>
+            </div>
+            <div className="price-part">
+              <div className="prices">
+                <p>Total Items</p> <span>{cartItems.length}</span>
+              </div>
+              <div className="prices">
+                <p>Subtotal</p> <span>${calculateSubtotal()}</span>
+              </div>
+              <div className="prices">
+                <p>Coupon Discount</p> <span>(-) {couponDiscount.toFixed(2)}</span>
+              </div>
+              <div className="prices">
+                <p>Shipping</p> <span className="shipping-price">$16.00</span>
+              </div>
+              <div className="shipping-fee">
+                <a href="/">View shipping charge</a>
+              </div>
+              <div className="total-price">
+                <p>Total</p> <span>${(Number(calculateSubtotal()) + 16).toFixed(2)}</span>
+              </div>
+            </div>
+            <div className="buttons">
+              <button className="checkout-button">Proceed To Checkout</button>
+              <a href="/" className="continue-shopping-link">
+                Continue Shopping
+              </a>
+            </div>
+          </div>
         </div>
       )}
     </div>
