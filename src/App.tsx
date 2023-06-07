720da36b74e9588ad96151dcf98dba8edef014f8
Orkhan <mammedov.01@mail.ru>
2023-06-07 17:12:12 +0400

    HomePage adaptivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import './App.css';
 import { CartProvider } from './pages/CartPage/components/CartContext';
-import Header from './shared/Header/Header';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes, useLocation } from 'react-router-dom';
 import CartPage from './pages/CartPage/CartPage';
 import Footer from './shared/Footer/Footer';
+import Header from './shared/Header/Header';
 import HomePage from './pages/HomePage/HomePage';
 import ProductViewPage from './pages/ProductViewPage/ProductViewPage';
-import React from 'react';
+import React, { useEffect, useLayoutEffect } from 'react';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
@@ -14,6 +24,7 @@ function App() {
       <CartProvider>
         <Router>
           <Header />
+          <ScrollToTop />
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/product/:productId" element={<ProductViewPage />} />
